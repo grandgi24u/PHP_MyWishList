4df9ff35b41eac37e8f56ae170e6cdc994fc13a3
grandgi24u <55753363+grandgi24u@users.noreply.github.com>
2021-01-13 21:36:55 +0100

    Ajout de la description sur la page d'accueil

--- src/vue/VuePrincipale.php
+++ src/vue/VuePrincipale.php
@@ -16,10 +16,18 @@ class VuePrincipale
    }
 
    public function accueil() : String {
-
+       $url_creerlistes = $this -> container -> router -> pathFor ( 'creerliste' );
        $html = <<<END
 
 <center><h1>Bienvenue sur le site MyWishList</h1></center>
+
+<p>MyWishList est une application en ligne pour créer, partager et gérer des listes de cadeaux.
+L'application permet de créer une liste de souhaits à l'occasion d'un événement
+particulier (anniversaire, fin d'année, mariage, retraite …) et lui permet de diffuser cette liste de
+souhaits à un ensemble de personnes concernées. Vous pouvez donc consulter cette liste
+et s'engager à offrir 1 élément de la liste. 
+</p>
+<a class='button' href="$url_creerlistes">Créer une liste</a>
 
 END;
         return $html;
