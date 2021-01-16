1a3ff6a02512bf597f6cca97beb5714aeaabdfc2
AurelienBls <aurelien.bls88@gmail.com>
2021-01-16 15:35:39 +0100

    Ajout commentaire de VueSession

--- src/vue/VueSession.php
+++ src/vue/VueSession.php
@@ -37,7 +37,7 @@ FIN;
     //Cette methode genere le formulaire de connexion
     private function connexion(): string
     {
-        //Definition du path qui appelle un nouvelle tentative de connexion
+        //Definition du path qui appelle une nouvelle tentative de connexion
         $url_testpass = $this -> container -> router -> pathFor ( 'testerConnexion' );
         //definition du formulaire html qui recupere les informations de connexion
         $html = <<<FIN
@@ -81,10 +81,12 @@ FIN;
         //ou l'alerte
         return $html;
     }
-
+    //Cette methode contient le formulaire de modification de compte
     private function modifierCompte(): string
     {
+        //Definition du path qui appelle une nouvelle tentative de modificationn de compte
         $url = $this -> container -> router -> pathFor ( 'modifierCompteA' );
+        //Contenue html de la vue Session lorsque l'utilisateur souhaite modifier son compte
         $html = <<<FIN
 <form method="POST" action="$url">
 	<br><label>Nom :<br> <input type="text" name="nom" value="{$this->tab['nom']}"/></label><br>
@@ -97,52 +99,67 @@ FIN;
 	<button class="button" type="submit">Enregistrer</button>
 </form>	
 FIN;
+        //retourne le formulaire
         return $html;
     }
-
+    //Cette Methode envoie la reponse de la vu en fonction du paramettre select
     public function render(int $select): string
     {
-
+        //switch sur le paramettre select
         switch ($select) {
+            //Si select vaut 0
             case 0 :
             {
+                //Le contenue est le formulaire d'enregistrement
                 VuePrincipale ::$content = $this -> formEnregistrement ();
                 break;
             }
+            //Si select vaut 1
             case 1 :
             {
+                //Le contenue est le message de validation
                 VuePrincipale ::$content = 'Votre compte avec le login <b>' . $this -> tab['login'] . '</b> a été créé';
                 break;
             }
+            //Si select vaut 2
             case 2 :
             {
+                //Le contenue est un message qui dit qu'il est deja connecte
                 VuePrincipale ::$content = "<h1>Vous etes déjà connecté</h1>";
                 break;
             }
+            //Si select vaut 3
             case 3 :
             {
+                //Le contenue est le formulaire de connexion
                 VuePrincipale ::$content = $this -> connexion ();
                 break;
             }
+            //Si select vaut 4
             case 4 :
             {
+                //Le contenue est un message d'erreur d'idantification
                 ($this -> tab['res']) ? VuePrincipale ::$content = 'Vous etes <b>connecté</b>' : VuePrincipale ::$content = 'Votre mot de passe est <b>Incorrect</b>';
                 break;
             }
+            //Si select vaut 5
             case 5 :
             {
+                //Le contenue est la liste des information du compte
                 VuePrincipale ::$content = $this -> compte ();
                 break;
             }
+            //Si select vaut 6
             case 6 :
             {
+                //Le contenue est le formulaire de creation de compte
                 VuePrincipale ::$content = $this -> modifierCompte ();
                 break;
             }
         }
-
+        //on vide la variable inMenu
         VuePrincipale::$inMenu = "";
-
+        //on retourn egalement le script index.php qui contient principalement le css
         return substr(include ("html/index.php"), 1,-1);
     }
 
