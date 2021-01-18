4dead14ef9966982a9f7ec0b3e0cd2895068db8c
AurelienBls <aurelien.bls88@gmail.com>
2021-01-18 11:53:57 +0100

    debut VueListe commentée

--- src/vue/VueListe.php
+++ src/vue/VueListe.php
@@ -22,11 +22,16 @@ class VueListe extends VuePrincipale
         parent ::__construct ( $t, $c );
     }
 
+    //Cette methode genere la liste des boutons qui mene vers les listes
     private function menulistesPubliques(): string
     {
+        //Definition du chemin qui mene vers les listes publiques
         $url_listes = $this -> container -> router -> pathFor ( 'afficherlistes' );
+        //Definition du chemin qui mene vers les listes expires
         $url_listesexpire = $this -> container -> router -> pathFor ( 'afficherlistesexpire' );
+        //Definition du chemin qui mene vers la page de creation de liste
         $url_creerlistes = $this -> container -> router -> pathFor ( 'creerliste' );
+        //On definie l'html du menu
         $html = <<<END
 <div class="vertical-menu">
   <a class="active">Les listes publiques</a>
@@ -35,15 +40,22 @@ class VueListe extends VuePrincipale
   <a href="$url_creerlistes">Créer une liste</a>
 </div>
 END;
+        //retoure l'html
         return $html;
     }
-
+    //Cette methode genere la liste des boutons qui mene vers les listes
+    //lorqu'un utilisateur est connecter
     private function menuMesListes(): string
     {
+        //Definition du chemin qui mene vers les listes de l'utilisateur
         $url_listes = $this -> container -> router -> pathFor ( 'affichermeslistes' );
+        //Definition du chemin qui mene vers les listes expires de l'utilisateur
         $url_listesexpire = $this -> container -> router -> pathFor ( 'affichermeslistesexpire' );
+        //Definition du chemin qui mene vers la page de creation de liste
         $url_creerlistes = $this -> container -> router -> pathFor ( 'creerliste' );
+        //Definition du chemin qui mene vers la page pour ajouter une liste
         $url_ajouteruneliste = $this -> container -> router -> pathFor ( 'ajouterUneListe' );
+        //On definie l'html du menu
         $html = <<<END
 <div class="vertical-menu">
   <a class="active">Mes listes</a>
@@ -53,102 +65,163 @@ END;
   <a href='$url_ajouteruneliste'>Ajouter une liste au compte</a>
 </div>
 END;
+        //retoure l'html
         return $html;
     }
 
+    //Creer la vue des listes publiques en cours
     private function lesListes(): string
     {
+        //Titre de la page
         $html = '<h2>Les listes en cours : </h2>';
 
+        //Si il y a des listes dans le tableau
         if (sizeof ( $this -> tab ) > 0) {
+            //Definition de la forme html d'une liste
+            //Correspond au nom de colonne du tableau
             $html .= "<table class='styled-table' ><thead><tr><td>Acces</td><td>Titre</td><td>Description</td><td>Date d'expiration</td><td>Code de partage</td><td>Action</td></tr></thead><tbody>";
+            //pour chaque liste dans le tableau
             foreach ($this -> tab as $liste) {
+                //si la liste est publique
                 if ($liste['acces'] == "public") {
+                    //Definition du chemin qui mene a la liste
                     $url_show = $this -> container -> router -> pathFor ( 'afficherUneListe', ['token' => $liste['token']] );
+                    //ON remplie l'html avec les information de la liste
                     $html .= "<tr><td>{$liste['acces']}</td><td>{$liste['titre']}</td> <td>{$liste['description']}</td> <td>{$liste['expiration']}</td>
                             <td>{$liste['token']}</td><td><a href='$url_show'><i class='fa fa-eye'></i></a></td></tr>";
                 }
             }
+            //Ferme les balises html
             $html .= "</tbody></table>";
         } else {
+            //Si il n'y a pas de liste dans le tableau
+            //on affiche
             $html .= "Aucune liste";
         }
+        //on place l'html dans des balises de liste
         $html = "<ul>$html</ul>";
+        //on retourne le resultat
         return $html;
     }
 
+    //Creer la vue des listes le l'utilisateur en cours
     private function meslistes(): string
     {
+        //Titre de la page
         $html = '<h2>Vos listes en cours : </h2>';
+        //Si il y a des listes dans le tableau
         if (sizeof ( $this -> tab ) > 0) {
+            //Definition de la forme html d'une liste
+            //Correspond au nom de colonne du tableau
             $html .= "<table class='styled-table' ><thead><tr><td>Acces</td><td>Titre</td><td>Description</td><td>Date d'expiration</td><td>Code de partage</td><td>Action</td></tr></thead><tbody>";
+            //pour chaque liste dans le tableau
             foreach ($this -> tab as $liste) {
+                //si la liste est privee
                 if (isset( $_SESSION['iduser'] )) {
+                    //Definition du chemin qui mene a la liste
                     $url_show = $this -> container -> router -> pathFor ( 'afficherUneListe', ['token' => $liste['token']] );
+                    //ON remplie l'html avec les information de la liste
                     $html .= "<tr><td>{$liste['acces']}</td><td>{$liste['titre']}</td> <td>{$liste['description']}</td> <td>{$liste['expiration']}</td>
                                     <td>{$liste['token']}</td><td><a href='$url_show'><i class='fa fa-eye'></i></a>";
+                    //Definition du chemin qui mene a la modification de la liste
                     $url_modif = $this -> container -> router -> pathFor ( 'listemodif', ['tokenModif' => $liste['tokenModif']] );
+                    //Definition du chemin qui mene a la suppression de la liste
                     $url_suppr = $this -> container -> router -> pathFor ( 'supprimerliste', ['tokenModif' => $liste['tokenModif']] );
+                    //on les implementes dans les information de la liste sous les icones
                     $html .= " <a href='$url_modif'><i class='fa fa-edit'></i></a>
                                       <a href='$url_suppr'><i class='fa fa-trash'></i></a></td></tr>";
                 }
+                //Ferme les balises html
                 $html .= "</tbody></table>";
             }
         } else {
+            //Si il n'y a pas de liste dans le tableau
+            //on affiche
             $html .= "Aucune liste";
         }
+        //on place l'html dans des balises de liste
         $html = "<ul>$html</ul>";
-
+        //on retourne le resultat
         return $html;
     }
 
+    //Creer l'affichage des listes expirees
     private function lesListesexpire(): string
     {
+        //Titre de la page
         $html = '<h2>Les listes expirées : </h2>';
-
+        //Si il y a des listes dans le tableau
         if (sizeof ( $this -> tab ) > 0) {
+            //Definition de la forme html d'une liste
+            //Correspond au nom de colonne du tableau
             $html .= "<table class='styled-table' ><thead><tr><td>Acces</td><td>Titre</td><td>Description</td><td>Date d'expiration</td><td>Code de partage</td><td>Action</td></tr></thead><tbody>";
+            //pour chaque liste dans le tableau
             foreach ($this -> tab as $liste) {
+                //si la liste est publique
                 if ($liste['acces'] == "public") {
+                    //Definition du chemin qui mene a la liste
                     $url_show = $this -> container -> router -> pathFor ( 'afficherUneListe', ['token' => $liste['token']] );
+                    //on remplie l'html avec les information de la liste
                     $html .= "<tr><td>{$liste['acces']}</td><td>{$liste['titre']}</td> <td>{$liste['description']}</td> <td>{$liste['expiration']}</td>
                             <td>{$liste['token']}</td><td><a href='$url_show'><i class='fa fa-eye'></i></a></td></tr>";
                 }
             }
+            //Ferme les balises html
             $html .= "</tbody></table>";
         } else {
+            //Si il n'y a pas de liste dans le tableau
+            //on affiche
             $html .= "Aucune liste";
         }
+        //on place l'html dans des balises de liste
         $html = "<ul>$html</ul>";
+        //on retourne le resultat
         return $html;
     }
 
+    //Creer l'affichage des listes expirees de l'utilisateur
     private function meslistesexpires(): string
     {
+        //Titre de la page
         $html = '<h2>Vos listes expirées : </h2>';
+        //Si il y a des listes dans le tableau
         if (sizeof ( $this -> tab ) > 0) {
+            //Definition de la forme html d'une liste
+            //Correspond au nom de colonne du tableau
             $html .= "<table class='styled-table' ><thead><tr><td>Acces</td><td>Titre</td><td>Description</td><td>Date d'expiration</td><td>Code de partage</td><td>Action</td></tr></thead><tbody>";
+            //pour chaque liste dans le tableau
             foreach ($this -> tab as $liste) {
+                //si la liste est privee
                 if (isset( $_SESSION['iduser'] )) {
+                    //Definition du chemin qui mene a la liste
                     $url_show = $this -> container -> router -> pathFor ( 'afficherUneListe', ['token' => $liste['token']] );
+                    //on remplie l'html avec les information de la liste
                     $html .= "<tr><td>{$liste['acces']}</td><td>{$liste['titre']}</td> <td>{$liste['description']}</td> <td>{$liste['expiration']}</td>
                                     <td>{$liste['token']}</td><td><a href='$url_show'><i class='fa fa-eye'></i></a>";
+                    //Definition du chemin qui mene a la suppression de la liste
                     $url_suppr = $this -> container -> router -> pathFor ( 'supprimerliste', ['tokenModif' => $liste['tokenModif']] );
+                    //on les implementes dans les information de la liste sous les icones
                     $html .= " <a href='$url_suppr'><i class='fa fa-trash'></i></a></td></tr>";
                 }
             }
+            //Ferme les balises html
             $html .= "</tbody></table>";
         } else {
+            //Si il n'y a pas de liste dans le tableau
+            //on affiche
             $html .= "Aucune liste";
         }
+        //on retourne le resultat
         return $html;
     }
-
+    //Creer la page de creation de liste
     private function creerliste(): string
     {
+        //Definition du format de la date
         $today = date ( 'Y-m-d' );
+        //Definition du chemin qui mene a la creation d'une nouvelle liste
         $url_new_liste = $this -> container -> router -> pathFor ( 'nouvelleliste' );
-
+        //on creer l'html qui met en forme le formulaire de creation
         $html = <<<FIN
 <h1>Créer une liste</h1>
 <form method="POST" action="$url_new_liste">
@@ -159,17 +232,24 @@ END;
 	<button class="button" type="submit">Enregistrer la liste</button>
 </form>	
 FIN;
+        //on retourne le formulaire html
         return $html;
     }
-
+    //Creer le formulaire de modification de liste
     private function modifliste(): string
     {
+        //Chemin qui appelle a une modification de liste
         $url = $this -> container -> router -> pathFor ( 'modifierliste', ['tokenModif' => $this -> tab['tokenModif']] );
+        //si la liste est publique
         if ($this -> tab['acces'] == "public") {
+            //on affiche la checkbox checker
             $a = "<label>Liste publique?</label><input type='checkbox' name='etat' value='yes' checked><br><br>";
         } else {
+            //si elle est privee
+            //on affiche la checkbox non checker
             $a = "<label>Liste publique?</label><input type='checkbox' name='etat' value='yes'><br><br>";
         }
+        //on definie le reste du formulaire
         $html = <<<FIN
 <h1>Modifier une liste</h1>
 <form method="POST" action="$url">
@@ -180,24 +260,32 @@ FIN;
 	<button class="button" type="submit">Enregistrer la modification</button>
 </form>	
 FIN;
+        //retourne le formulaire html
         return $html;
     }
 
+    //creer la page d'ajout de liste
     private function ajouterUneListe(): string
     {
+        //chemin qui appelle un ajout de liste
         $url = $this -> container -> router -> pathFor ( 'sajouterUneListe' );
+        //creation du formulaire et des boutons
         $html = <<<FIN
 <form method="POST" action="$url">
 	<label>Code de modification :<br> <input type="text" name="token" required/></label><br>
 	<button class="button" type="submit">Ajouter</button>
 </form>	
 FIN;
+        //retourne le formulaire html
         return $html;
     }
 
+    //creer la page qui donne le token de modification de la liste
     private function donnerTokenModif(): string
     {
+        //chemin qui applelle la page de modification de liste
         $url_additem = $this -> container -> router -> pathFor ( 'afficherUneListeWithModif', ['tokenModif' => $this -> tab['tokenModif']] );
+        //creation le page
         $html = <<<END
 <h1>Informations importantes : </h1>
 <p>
@@ -207,9 +295,11 @@ Et voici votre code de patarge a donner a vos amis / familles / etc : {$this -> 
 </p>
 <br><a class='button' href='$url_additem'>Accéder en modification à la liste</a>
 END;
+        //retourne l'html de la page
         return $html;
     }
 
+    //
     private function uneListe(): string
     {
         if(isset($_COOKIE['liste']) && in_array ($this->tab['tokenModif'],$_COOKIE['liste'])){
